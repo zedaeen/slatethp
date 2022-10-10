53d620ace2c8f963d38e44bdfdb1df55a9929e49
zaen akbar <zentzaen@gmail.com>
2022-10-11 00:33:57 +0700

    fix some display name

--- components/components.tsx
+++ components/components.tsx
@@ -8,82 +8,89 @@ interface BaseProps {
 }
 type OrNull<T> = T | null
 
-export const Button = React.forwardRef(
-  (
-    {
-      className,
-      active,
-      reversed,
-      ...props
-    }: PropsWithChildren<
-      {
-        active: boolean
-        reversed: boolean
-      } & BaseProps
-    >,
-    ref: Ref<OrNull<HTMLSpanElement>>
-  ) => (
-    <span
-      {...props}
-      ref={ref}
-      className={cx(
-        className,
-        css`
-          cursor: pointer;
-          color: ${reversed
-            ? active
-              ? 'white'
-              : '#aaa'
-            : active
-            ? '#8e7cc3'
-            : '#ccc'};
-        `
-      )}
-    />
-  )
-)
+export function Button() {
+    return React.forwardRef(
+        (
+          {
+            className,
+            active,
+            reversed,
+            ...props
+          }: PropsWithChildren<
+            {
+              active: boolean
+              reversed: boolean
+            } & BaseProps
+          >,
+          ref: Ref<OrNull<HTMLSpanElement>>
+        ) => (
+          <span
+            {...props}
+            ref={ref}
+            className={cx(
+              className,
+              css`
+                cursor: pointer;
+                color: ${reversed
+                  ? active
+                    ? 'white'
+                    : '#aaa'
+                  : active
+                  ? '#8e7cc3'
+                  : '#ccc'};
+              `
+            )}
+          />
+        )
+      )
+} 
 
-export const Menu = React.forwardRef(
-  (
-    { className, ...props }: PropsWithChildren<BaseProps>,
-    ref: Ref<OrNull<HTMLDivElement>>
-  ) => (
-    <div
-      {...props}
-      ref={ref}
-      className={cx(
-        className,
-        css`
-          & > * {
-            display: inline-block;
-          }
-          & > * + * {
-            margin-left: 15px;
-          }
-        `
-      )}
-    />
-  )
-)
+export function Menu() {
+    return React.forwardRef(
+        (
+          { className, ...props }: PropsWithChildren<BaseProps>,
+          ref: Ref<OrNull<HTMLDivElement>>
+        ) => (
+          <div
+            {...props}
+            ref={ref}
+            className={cx(
+              className,
+              css`
+                & > * {
+                  display: inline-block;
+                }
+                & > * + * {
+                  margin-left: 15px;
+                }
+              `
+            )}
+          />
+        )
+      )
+} 
 
-export const Toolbar = React.forwardRef(
-  (
-    { className, ...props }: PropsWithChildren<BaseProps>,
-    ref: Ref<OrNull<HTMLDivElement>>
-  ) => (
-    <Menu
-      {...props}
-      ref={ref}
-      className={cx(
-        className,
-        css`
-          position: relative;
-          padding: 1px 18px 17px;
-          margin: 0 -20px;
-          border-bottom: 2px solid #eee;
-          margin-bottom: 20px;
-        `
-      )}
-    />
-  )
-)
+export function Toolbar() {
+    const MenuComponent = Menu()
+    return React.forwardRef(
+        (
+          { className, ...props }: PropsWithChildren<BaseProps>,
+          ref: Ref<OrNull<HTMLDivElement>>
+        ) => (
+          <MenuComponent
+            {...props}
+            ref={ref}
+            className={cx(
+              className,
+              css`
+                position: relative;
+                padding: 1px 18px 17px;
+                margin: 0 -20px;
+                border-bottom: 2px solid #eee;
+                margin-bottom: 20px;
+              `
+            )}
+          />
+        )
+      )
+}
